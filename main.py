b7155f57cfbf417afd4d5b30730b04ce00369722
LauraSimpson <laura.simpson@mycit.ie>
2021-05-31 21:13:24 +0100

    NYC SALES UPLOADED

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
 # See PyCharm help at https://www.jetbrains.com/help/pyc
-import pandas
+import pandas as pd
 
-data = pandas.read_csv("nyc-rolling-sales.csv")
+df = pd.read_csv("nyc-rolling-sales.csv")
+print(df)
+
+data = pd.read_csv("nyc-rolling-sales.csv")
 print(data)
+
+import pandas as pd
+df = pd.DataFrame(data, columns= ['BOROUGH','SALE Price', 'SALE DATE'])
+print (df)
+
+import pandas as pd
+df = pd.DataFrame(data, columns= ['NEIGHBOURHOOD','SALE Price', 'SALE DATE'])
+print (df)
+
+import pandas as pd
+df = pd.DataFrame(data, columns= ['category','SALE Price', 'SALE DATE'])
+print (df)
+
+import pandas as pd
+df = pd.DataFrame(columns= ['category', 'sale Price','sale date'])
+df.sort_values(by=['category'], inplace=True)
+print (df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
