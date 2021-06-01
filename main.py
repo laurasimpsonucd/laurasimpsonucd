b022974364a707f96be9f1d77bc914694797304b
LauraSimpson <laura.simpson@mycit.ie>
2021-06-01 17:29:31 +0100

    sorting data

--- main.py
+++ main.py
@@ -24,6 +24,55 @@ df = pd.DataFrame(columns= ['category', 'sale Price','sale date'])
 df.sort_values(by=['category'], inplace=True)
 print (df)
 
+import pandas as pd
+
+data = {'First Column Name':  ['First value', 'Second value',...],
+        'Second Column Name': ['First value', 'Second value',...]}
+
+df = pd.DataFrame (data, columns = ['First Column Name','Second Column Name',...])
+
+print (df)
+
+import pandas as pd
+
+property = {'neighbourhood': ['alphabet city','Chelsea','chinatown','civic center'],
+        'sale Price': [662500,3936272,8000000,11693337]
+        }
+
+df = pd.DataFrame(property, columns = ['neighbourhood', 'sale Price'])
+
+print (df)
+
+import pandas as pd
+
+property = {'neighbourhood': ['alphabet city','Chelsea','chinatown','civic center'],
+        'sale Price': [662500,3936272,8000000,11693337]
+        }
+
+df = pd.DataFrame( property, columns = ['neighbourhood',' sale Price'], index=['property_1','property_2','property_3','property_4'])
+
+print (df)
+
+import pandas as pd
+
+property = {'neighbourhood': ['alphabet city','Chelsea','chinatown','civic center'],
+        'Price': [662500,3936272,8000000,11693337],
+        'sale date': [2016, 2017,2016,2017]
+        }
+
+df = pd.DataFrame(property, columns=['neighbourhood', 'sale Price', 'sale date'])
+
+print(df)
+
+
+
+
+
+
+
+
+
+
 
 
 
